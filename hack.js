6ae67dbf061a39e64ee315d98b631025b74b5783
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2023-08-18 21:07:20 +0200

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,3 +1,4 @@
 'use strict'
 document.getElementById('dialog-window').style.width = '100%';
 document.getElementsByTagName('img')[1].src = 'https://media.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif';
+document.getElementsByTagName('img')[1].style.marginRight = '8px';
