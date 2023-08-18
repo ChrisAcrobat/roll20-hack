477ba76caad12a58aa12d90f8d041e9ed1ad4b1b
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2023-08-18 21:02:30 +0200

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,3 +1,4 @@
 'use strict'
 document.getElementById('dialog-window').style.width = '100%';
+document.getElementsByTagName('img')[1].src = '';
 document.getElementsByTagName('img')[1].innerHTML = '<div class="tenor-gif-embed" data-postid="22954713" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713">Rickroll Never Gonna Give You Up GIF</a>from <a href="https://tenor.com/search/rickroll-gifs">Rickroll GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>';
